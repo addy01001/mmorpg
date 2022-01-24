42dea053d539a3fbe6134e935ceea43a5729e77d
addy01001 <77636542+addy01001@users.noreply.github.com>
2022-01-24 18:28:13 +0530

    changes

--- src/bot/bot.go
+++ src/bot/bot.go
@@ -8,7 +8,7 @@ import (
 )
 
 var BotId string
-var goBot *discordgo.Session
+// var goBot *discordgo.Session
 
 func Start() {
 	//creating new bot session
@@ -19,19 +19,21 @@ func Start() {
 		fmt.Println(err.Error())
 		return
 	}
+
 	// Making our bot a user using User function .
 	u, err := goBot.User("@me")
+
 	//Handlinf error
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
+
 	// Storing our id from u to BotId .
 	BotId = u.ID
 
 	// Adding handler function to handle our messages using AddHandler from discordgo package. We will declare messageHandler function later.
 	goBot.AddHandler(messageHandler)
-
 
 	err = goBot.Open()
 	//Error handling
@@ -45,12 +47,12 @@ func Start() {
 
 //Definition of messageHandler function it takes two arguments first one is discordgo.Session which is s , second one is discordgo.MessageCreate which is m.
 func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	//Bot musn't reply to it's own messages , to confirm it we perform this check.
 	if m.Author.ID == BotId {
 		return
 	}
-	//If we message ping to our bot in our discord it will return us pong .
-	if m.Content == "ping" {
-		_, _ = s.ChannelMessageSend(m.ChannelID, "pong")
+
+	if m.Content == "bot help" {
+		msg,_:=s.ChannelMessageSend(m.ChannelID, "You have pressed help\n\nCommands:\nstart - start game\nstop - stop game\nswitch - switch to Co-op/Single-player")
+		s.MessageReactionAdd(msg.ChannelID, msg.ID,"neutral_face")
 	}
 }
